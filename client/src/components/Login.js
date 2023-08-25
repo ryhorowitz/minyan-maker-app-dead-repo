002a80da8fbff1086adbef4d87808d70a03c9e00
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-25 14:04:48 -0400

    adding bootstrap

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -88,6 +88,24 @@ function Login() {
 
   return (
     <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <h2>Login</h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" className="form-control" id="email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" />
+              </div>
+              <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
       <h1>Login</h1>
 
       <form onSubmit={handleLogin}>
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AppProvider from './AppProvider';
-
+import "./index.css"
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
