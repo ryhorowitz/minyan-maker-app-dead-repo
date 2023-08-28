4530bb5cf6ccb976a40250d79923690a3d37b274
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-08-27 20:40:32 -0400

    need to work on styling the max size of the shul cards

--- client/src/components/Shuls.js
+++ client/src/components/Shuls.js
@@ -4,22 +4,30 @@ import AppContext from "../AppContext"
 function Shuls() {
   const { shuls, setShuls } = useContext(AppContext)
 
+  const ShuldGridStyle = {
+    maxWidth: "40rem",
+    Height: "60rem"
+  }
   const shulList = shuls.map(shul => {
-    return <div className="card" key={shul.id}>
-      <img src={shul.img} class="card-img-top" alt={shul.name}></img>
-      <div className="card-body">
-        <h5 class="card-title">{shul.name}</h5>
-        <div>
-          <p class="card-text">{shul.street_address} {shul.city}, {shul.state} {shul.postal_code}</p>
+    return <div className="col">
+
+      <div className="card" key={shul.id}>
+        <img src={shul.img} class="card-img-top" alt={shul.name}></img>
+        <div className="card-body">
+          <h5 class="card-title">{shul.name}</h5>
+          <p class="text-end">{shul.street_address} {shul.city}, {shul.state} {shul.postal_code}</p>
         </div>
       </div>
     </div>
   })
+  const shulGrid = <div class="row row-cols-1 row-cols-md-2 g-4"
+    style={ShuldGridStyle}
+  >{shulList}</div>
   // create shul card
   return (
     <>
       Shuls List
-      <ol>{shulList}</ol>
+      <ol>{shulGrid}</ol>
     </>
   )
 }
